b62fae635dc0fce50a2c63ad031257b0a477b85e
mclaire19 <madeline.pickens1@gmail.com>
2020-01-30 21:29:37 -0500

    removing extra data

---
+++ DataWrangling/Wrangling_Starter_Script.R
@@ -0,0 +1,10 @@
+## necessary packages
+
+# if needed: install.packages('tidyverse')
+library(tidyverse)
+
+## import data
+
+# how does readr parse one of these files?
+
+read_csv('data/fafsa-demographics-2017-18.csv')
